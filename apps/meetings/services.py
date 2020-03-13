3b575c21db4b52f351d520476b45df2675572e95
14-10526 <14-10526@usb.ve>
2020-03-12 20:05:07 -0400

    fixing servicios

--- apps/meetings/services.py
+++ apps/meetings/services.py
@@ -12,14 +12,15 @@ MODULE = 'Reunion'
 
 
 ''' Retorna fecha de reunion de planning y lista de resultados de un sprint '''
-@app.route("/meetings/plannings/<int:sprint_id>")
+@app.route("/meetings/plannings/<int:sprint_id>",methods=['GET'])
 def get_planning_by_sprint(sprint_id):
-    planning = Planning.get_or_404(sprint_id=sprint_id)
-    dic = [planning.serialize()]
-    results = PlanningResults.query.filter_by(planning_id=planning.id)
-    if results.count() > 0:
-        dic.extend([result.serialize() for result in results])
-    return jsonify(dic)
+    if request.method == 'GET':
+        planning = Planning.get_or_404(sprint_id=sprint_id)
+        dic = [planning.serialize()]
+        results = PlanningResults.query.filter_by(planning_id=planning.id)
+        if results.count() > 0:
+            dic.extend([result.serialize() for result in results])
+        return jsonify(dic)
 
 
 ''' Agregar resultado de planning '''
@@ -35,13 +36,14 @@ def get_planning_by_sprint(sprint_id):
 
 
 ''' Listar todas las reuniones de retrospectiva de un sprint '''
-@app.route("/meetings/retrospectives/<int:sprint_id>")
+@app.route("/meetings/retrospectives/<int:sprint_id>",methods=['GET'])
 def get_retrospectives_by_sprint(sprint_id):
-    meetings = Retrospective.query.filter_by(sprint_id=sprint_id)
-    if meetings.count() > 0:
-        return jsonify([meeting.serialize() for meeting in meetings])
-    else:
-        return jsonify({'server': 'NO_CONTENT'})
+    if request.method == 'GET':
+        meetings = Retrospective.query.filter_by(sprint_id=sprint_id)
+        if meetings.count() > 0:
+            return jsonify([meeting.serialize() for meeting in meetings])
+        else:
+            return jsonify({'server': 'NO_CONTENT'})
 
 
 ''' Agregar  reunion de retrospectiva '''
@@ -57,22 +59,23 @@ def get_retrospectives_by_sprint(sprint_id):
 
 
 ''' Listar todos los dailies de un sprint '''
-@app.route("/meetings/dailies/<int:sprint_id>")
+@app.route("/meetings/dailies/<int:sprint_id>",methods=['GET'])
 def get_dailies_by_sprint(sprint_id):
-    meetings = Daily.query.filter_by(sprint_id=sprint_id)
-    if meetings.count() > 0:
-        return jsonify([meeting.serialize() for meeting in meetings])
-    else:
-        return jsonify({'server': 'NO_CONTENT'})
+    if request.method == 'GET':
+        meetings = Daily.query.filter_by(sprint_id=sprint_id)
+        if meetings.count() > 0:
+            return jsonify([meeting.serialize() for meeting in meetings])
+        else:
+            return jsonify({'server': 'NO_CONTENT'})
 
 
 ''' Agregar daily '''
 @app.route("/meetings/dailies/add", methods=['POST'])
 def add_daily():
     if request.method == 'POST':
-        date = request.json.get('date')
-        report = request.json.get('report')
-        sprint_id = request.json.get('sprint_id')
+        date = request.form.get('date')
+        report = request.form.get('report')
+        sprint_id = request.form.get('sprint_id')
 
         try:
             daily = Daily(
@@ -84,62 +87,60 @@ def add_daily():
             db.session.commit()
 
             ###########Agregando evento al logger#######################
-            add_event_logger(sprint_id, LoggerEvents.add_daily, MODULE)
+            #add_event_logger(sprint_id, LoggerEvents.add_daily, MODULE)
             ############################################################
-            return jsonify(daily.serialize()), 200
-        except Exception as e:
-            print(e)
-            return jsonify({'server': e})
+            return jsonify(daily.serialize())
+        except:
+            return jsonify({'ERROR':'500_INTERNAL_SERVER_ERROR'})
 
 
 ''' Buscar daily '''
-
-@app.route("/daily/search/<int:id_>")
+@app.route("/meetings/dailies/search/<int:id_>",methods=['GET'])
 def search_daily(id_):
-    try:
+    if request.method == 'GET':
         daily= Daily.query.get_or_404(id_)
-
-        daily_id=daily.daily_id
         ###########Agregando evento al logger###########################
         #add_event_logger(daily_id, LoggerEvents.search_daily, MODULE)
         ################################################################
-
-        return  jsonify([daily.serialize()])
-    except:
-         return jsonify({'server': 'ERROR'})
+        return  jsonify(daily.serialize())
 
 
 ''' Modificar daily '''
-@app.route("/daily/update/<daily_id>", methods=['PUT'])
-def update_daily(daily_id):
+@app.route("/meetings/dailies/<id_>", methods=['PUT'])
+def update_daily(id_):
     if request.method == 'PUT':
-        daily = Daily.query.get_or_404(daily_id)
-        date = request.json.get('date')
-        report = request.json.get('report')
-        daily_id = request.json.get('daily_id')
+        daily = Daily.query.get_or_404(id_)
+        date = request.form.get('date')
+        report = request.form.get('report')
+        sprint_id = request.form.get('sprint_id')
+
         daily.date = date
         daily.report = report
-        daily.sprint_id = daily_id
+        daily.sprint_id = sprint_id
 
-        try:
-            db.session.commit()
+        db.session.commit()
 
-            ###########Agregando evento al logger##########################
-            add_event_logger(
-            daily.daily_id, LoggerEvents.update_daily, MODULE)
-            ###############################################################
+        ###########Agregando evento al logger##########################
+        #add_event_logger(
+        #daily.daily_id, LoggerEvents.update_daily, MODULE)
+        ###############################################################
 
-            return jsonify(daily.serialize())
-        except:
-            return jsonify({'server': 'ERROR'})
+        return jsonify(daily.serialize())
 
 
 ''' Eliminar daily '''
-@app.route("/meetings/dailies/delete/<daily_id>", methods=['POST'])
-def delete_daily(daily_id):
-    try:
-        daily = AcceptanceTest.query.filter_by(id=daily_id).delete()
-        db.session.commit()
-        return jsonify(daily), 200
-    except Exception as e:
-        return(str(e))
+@app.route("/meetings/dailies/delete/<id_>", methods= ['DELETE'])
+def delete_daily(id_):
+   if request.method == 'DELETE':
+        daily = Daily.query.get_or_404(id_)
+        try:
+            db.session.delete(daily)
+            db.session.commit()
+
+            ###########Agregando evento al logger###########################
+            #add_event_logger(user_id, LoggerEvents.delete_daily, MODULE)
+            ################################################################
+
+            return jsonify({'server': '200'})
+        except:
+            return jsonify({'server': 'ERROR'})
